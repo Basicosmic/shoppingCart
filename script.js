f97e39f19446d7875f8afe1bed53d700bae8f996
basicosmic <1297820x@gmail.com>
2021-06-12 01:01:46 -0700

    updating button

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var li1 = document.querySelectorAll("li")[0];
-var li2 = document.querySelectorAll("li")[1];
-var li3 = document.querySelectorAll("li")[2];
-var li4 = document.querySelectorAll("li")[3];
-var li5 = document.querySelectorAll("li")[4];
-var li6 = document.querySelectorAll("li")[5];
 
-
+document.querySelectorAll('li').forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.toggle("done")
+  })
+})
 
 function inputLength() {
 	return input.value.length;
@@ -33,34 +31,6 @@ function addListAfterKeypress(event) {
 	}
 }
 
-function toggleDone1(){
-	li1.classList.toggle("done");
-}
-function toggleDone2(){
-	li2.classList.toggle("done");
-}
-function toggleDone3(){
-	li3.classList.toggle("done");
-}
-function toggleDone4(){
-	li4.classList.toggle("done");
-}
-function toggleDone5(){
-	li5.classList.toggle("done");
-}
-function toggleDone6(){
-	li6.classList.toggle("done");
-}
-
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-li1.addEventListener("click", toggleDone1);
-li2.addEventListener("click", toggleDone2);
-li3.addEventListener("click", toggleDone3);
-li4.addEventListener("click", toggleDone4);
-li5.addEventListener("click", toggleDone5);
-li6.addEventListener("click", toggleDone6);
-
-
